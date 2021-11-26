b07c302dfb5cfefc89e5cbbab5501e56f3299e88
beyondr <jeopatto@gmail.com>
2021-11-26 11:23:55 -0500

    Update UI for public mint page

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,30 +1,27 @@
-import { Button, Container, FormControl, FormLabel, Input, FormHelperText } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Text, HStack, Stack } from '@chakra-ui/react';
 import { useState } from 'react';
 import Footer from '../components/Footer';
-import { SongADAOContractABI } from '../web3/constants';
 import { useContract, useReadContract, useWriteContract } from '../web3/hooks';
 import { useWallet } from '../web3/WalletContext';
-import { SongADay__factory } from './types';
+import { SongADay__factory } from '../types';
 
 export default function Mint() {
-  const [waiting, setWaiting] = useState(false);
+  const maxMint = 5;
+  const songCost = 0.2;
+
   const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
   const { contract } = useContract(contractAddress, SongADay__factory);
+
   const { address, provider } = useWallet();
-  // const { response, error } = useReadContract(contract, 'name');
+  const [mintAmount, setMintAmount] = useState(1);
+
   const { response: priceAmount } = useReadContract(contract, 'priceAmount');
 
-  // useEffect(() => {
-  //   const getName = async () => {
-  //     const testContract = await contract.name();
-  //     console.log(testContract);
-  //   };
-  //   getName();
-  // }, [contract?.address]);
+  // TODO: Set up loading ui when txn is being processed
+  const [waiting, setWaiting] = useState(false);
 
   const handleConfirmation = async () => {
-    // toast.success('Plasma Claimed');
-    console.log('Plasma Claimed');
+    console.log('Minted');
     setWaiting(false); // create waiting state variable - utilize modal
   };
 
@@ -35,6 +32,12 @@ export default function Mint() {
   const handleError = (error: any) => {
     console.log(error?.data?.message || error.message);
   };
+
+  // const [batchMint] = useWriteContract(contract, 'batchMint', {
+  //   onConfirmation: handleConfirmation, // when txn is confirmed on chain
+  //   onError: handleError, // txn had an err
+  //   onResponse: handleHarvested, // txn has been accepted on chain / in pool
+  // });
 
   const [mint] = useWriteContract(contract, 'publicMint', {
     onConfirmation: handleConfirmation, // when txn is confirmed on chain
@@ -48,46 +51,92 @@ export default function Mint() {
     onResponse: handleHarvested, // txn has been accepted on chain / in pool
   });
 
-  // Later call harvestPlasma
+  const SongNotes = () => {
+    const notes = new Array(maxMint).fill('🎙');
+    return (
+      <>
+        <HStack py={8}>
+          {notes.map((note, index) => {
+            return (
+              <Text key={index} fontSize={36} opacity={index < mintAmount ? '1' : '.2'}>
+                {note}
+              </Text>
+            );
+          })}
+        </HStack>
+      </>
+    );
+  };
 
-  // onClick = {() => {
-  //   try {
-  //   setWaiting(true)
-  // await harvestPlasma(..args)
-  // } catch(e) {
-  //   console.log(e)
-  //   setWaiting(false)
-  // }
-  // }}
-
-  // console.log('provider', provider);
-  // console.log('abi', SongADAOContractABI);
-  // console.log('address', address);
   console.log('price Amount', priceAmount?.toString());
-  // console.log('custom error', error);
-
   return (
     <>
-      <Container>
-        <FormControl id="email">
-          <FormLabel>Email address</FormLabel>
-          <Input type="number" />
-          <FormHelperText>We'll never share your email.</FormHelperText>
-
+      <Container centerContent p={8}>
+        <Stack textAlign="center">
+          <Heading>Mint random songs</Heading>
+          <Text>
+            Hit the big button and you’ll mint a random NFT from the pool of available songs. Each
+            song costs Ξ{songCost}, plus gas.
+          </Text>
+        </Stack>
+        <SongNotes />
+        <HStack spacing={8} pb={6}>
           <Button
+            variant="outline"
+            disabled={mintAmount === 1}
+            onClick={() => setMintAmount((prevMintAmount) => prevMintAmount - 1)}
+          >
+            ← Less
+          </Button>
+          <Heading>{mintAmount}</Heading>
+          <Button
+            variant="outline"
+            disabled={mintAmount === maxMint}
+            onClick={() => setMintAmount((prevMintAmount) => prevMintAmount + 1)}
+          >
+            More →
+          </Button>
+        </HStack>
+        <Box textAlign="center">
+          <Button
+            size="lg"
+            style={{ color: 'white' }}
             onClick={async () => {
               try {
                 setWaiting(true);
-                await mint(4, { value: priceAmount.mul(4) });
+                await mint(mintAmount, { value: priceAmount.mul(mintAmount) });
               } catch (e) {
                 console.log(e);
                 setWaiting(false);
               }
             }}
           >
-            MINT 🍬
+            Mint {mintAmount} {mintAmount === 1 ? 'song' : 'songs'}
           </Button>
+          <Text>Estimated cost: Ξ{(mintAmount * songCost).toPrecision(1)}</Text>
+          <Text fontSize="sm" opacity=".75">
+            (gas not included)
+          </Text>
+          {/* <Button
+            size="lg"
+            style={{ color: 'white' }}
+            onClick={async () => {
+              try {
+                setWaiting(true);
+                await batchMint(mintAmount, { value: priceAmount.mul(mintAmount) });
+              } catch (e) {
+                console.log(e);
+                setWaiting(false);
+              }
+            }}
+          >
+            Batch mint
+          </Button> */}
+        </Box>
+        <Box backgroundColor="gray.100" w="full" textAlign="center" my="4" p={4}>
+          <Text color="red">⚠ REMOVE FOR PROD ⚠</Text>
           <Button
+            variant="ghost"
             onClick={async () => {
               try {
                 setWaiting(true);
@@ -98,9 +147,9 @@ export default function Mint() {
               }
             }}
           >
-            FLIP SALE
+            Flip sale
           </Button>
-        </FormControl>
+        </Box>{' '}
       </Container>
       <Footer />
     </>
