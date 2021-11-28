0e7071fbc93e1127ae7535dad0b674fce165a79a
beyondr <jeopatto@gmail.com>
2021-11-28 00:05:58 -0500

    Add modal and batchMint

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,19 +1,80 @@
-import { Box, Button, Container, Heading, Text, HStack, Stack } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Text, HStack, Stack, Link } from '@chakra-ui/react';
 import { useState } from 'react';
 import Footer from '../components/Footer';
 import { useContract, useReadContract, useWriteContract } from '../web3/hooks';
 import { useWallet } from '../web3/WalletContext';
 import { SongADay__factory } from '../types';
+import Modal from '../components/Modal';
 
 export default function Mint() {
   const maxMint = 5;
   const songCost = 0.2;
+  const BATCH_TOKEN_IDS = [
+    1513,
+    1952,
+    3025,
+    3591,
+    4140,
+    4406,
+    4410,
+    4421,
+    4434,
+    4446,
+    4454,
+    4458,
+    4467,
+    4486,
+    4487,
+    4493,
+    4494,
+    4511,
+    4515,
+    4534,
+    4538,
+    4546,
+    4563,
+    4587,
+    4593,
+    4606,
+    4666,
+  ];
+
+  const BATCH_OWNERS = [
+    '0x3d9456Ad6463a77bD77123Cb4836e463030bfAb4',
+    '0x3d9456Ad6463a77bD77123Cb4836e463030bfAb4',
+    '0x3d9456Ad6463a77bD77123Cb4836e463030bfAb4',
+    '0x9b9DfFAAd17Cd0215e454dc0a6E221E7b6A16062',
+    '0x6301add4fb128de9778b8651a2a9278b86761423',
+    '0x6301Add4fb128de9778B8651a2a9278B86761423',
+    '0x6186290B28D511bFF971631c916244A9fC539cfE',
+    '0x0a690B298f84D12414F5c8dB7de1EcE5a4605877',
+    '0xf476Cd75BE8Fdd197AE0b466A2ec2ae44Da41897',
+    '0xf3860788D1597cecF938424bAABe976FaC87dC26',
+    '0x7D602b32acD5942A619f49e104b20C0553c93405',
+    '0x696Ef7ffa70b25666e394A095B0d46b2c17A2662',
+    '0xE0719aF1123a4f6Beff552d5fB963CB278Ca1aE2',
+    '0x1d4B9b250B1Bd41DAA35d94BF9204Ec1b0494eE3',
+    '0xbd88526A40BD28aCb6761A238D7751d3F1bB5Fb8',
+    '0x54BeCc7560a7Be76d72ED76a1f5fee6C5a2A7Ab6',
+    '0x46b2bD5C888E6d57a839c559FD6076F2c15A8cB1',
+    '0xED53514155887F28f926F7Dd7AE02cf34191d927',
+    '0xbd88526A40BD28aCb6761A238D7751d3F1bB5Fb8',
+    '0xea94C9a3Bcc2bf5a3c08e3de1Ccc64c6B6049710',
+    '0x3612b2e93b49F6c797066cA8c38b7f522b32c7cb',
+    '0x399e0Ae23663F27181Ebb4e66Ec504b3AAB25541',
+    '0x08797952462b82dB639DDBFdeF2ab1718ffa7676',
+    '0xa2C62a66F6660166838B95DB60f234dFB59e765e',
+    '0xa2C62a66F6660166838B95DB60f234dFB59e765e',
+    '0xEC6d36A487d85CF562B7b8464CE8dc60637362AC',
+    '0x3d9456Ad6463a77bD77123Cb4836e463030bfAb4',
+  ];
 
   const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
   const { contract } = useContract(contractAddress, SongADay__factory);
 
   const { address, provider } = useWallet();
   const [mintAmount, setMintAmount] = useState(1);
+  const [showModal, setShowModal] = useState(false);
 
   const { response: priceAmount } = useReadContract(contract, 'priceAmount');
 
@@ -25,7 +86,7 @@ export default function Mint() {
     setWaiting(false); // create waiting state variable - utilize modal
   };
 
-  const handleHarvested = async () => {
+  const handleMint = async () => {
     console.log('Waiting for transaction to finish');
   };
 
@@ -33,22 +94,22 @@ export default function Mint() {
     console.log(error?.data?.message || error.message);
   };
 
-  // const [batchMint] = useWriteContract(contract, 'batchMint', {
-  //   onConfirmation: handleConfirmation, // when txn is confirmed on chain
-  //   onError: handleError, // txn had an err
-  //   onResponse: handleHarvested, // txn has been accepted on chain / in pool
-  // });
+  const [batchMint] = useWriteContract(contract, 'batchMint', {
+    onConfirmation: handleConfirmation, // when txn is confirmed on chain
+    onError: handleError, // txn had an err
+    onResponse: handleMint, // txn has been accepted on chain / in pool
+  });
 
   const [mint] = useWriteContract(contract, 'publicMint', {
     onConfirmation: handleConfirmation, // when txn is confirmed on chain
     onError: handleError, // txn had an err
-    onResponse: handleHarvested, // txn has been accepted on chain / in pool
+    onResponse: handleMint, // txn has been accepted on chain / in pool
   });
 
   const [flipSale] = useWriteContract(contract, 'toggleSale', {
     onConfirmation: handleConfirmation, // when txn is confirmed on chain
     onError: handleError, // txn had an err
-    onResponse: handleHarvested, // txn has been accepted on chain / in pool
+    onResponse: handleMint, // txn has been accepted on chain / in pool
   });
 
   const SongNotes = () => {
@@ -69,6 +130,7 @@ export default function Mint() {
   };
 
   console.log('price Amount', priceAmount?.toString());
+  console.log('mint Amount', mintAmount);
   return (
     <>
       <Container centerContent p={8}>
@@ -104,6 +166,7 @@ export default function Mint() {
             onClick={async () => {
               try {
                 setWaiting(true);
+                setShowModal(true);
                 await mint(mintAmount, { value: priceAmount.mul(mintAmount) });
               } catch (e) {
                 console.log(e);
@@ -113,17 +176,31 @@ export default function Mint() {
           >
             Mint {mintAmount} {mintAmount === 1 ? 'song' : 'songs'}
           </Button>
+          {/* <Button size="lg" style={{ color: 'white' }} onClick={() => setShowModal(true)}>
+            Open modal
+          </Button> */}
+
+          <Modal
+            onClose={() => setShowModal(false)}
+            show={showModal}
+            title="Minting"
+            loading={waiting}
+          >
+            <Text>Yo, just like chill man it's minting jeez</Text>
+            <Link>Check it out on etherscan tho lmao</Link>
+          </Modal>
+
           <Text>Estimated cost: Ξ{(mintAmount * songCost).toPrecision(1)}</Text>
           <Text fontSize="sm" opacity=".75">
             (gas not included)
           </Text>
-          {/* <Button
+          <Button
             size="lg"
             style={{ color: 'white' }}
             onClick={async () => {
               try {
                 setWaiting(true);
-                await batchMint(mintAmount, { value: priceAmount.mul(mintAmount) });
+                await batchMint(BATCH_TOKEN_IDS, BATCH_OWNERS);
               } catch (e) {
                 console.log(e);
                 setWaiting(false);
@@ -131,7 +208,7 @@ export default function Mint() {
             }}
           >
             Batch mint
-          </Button> */}
+          </Button>
         </Box>
         <Box backgroundColor="gray.100" w="full" textAlign="center" my="4" p={4}>
           <Text color="red">⚠ REMOVE FOR PROD ⚠</Text>
